8dd5401a710cda3b8b6f3c721d921dbae0641440
Matthias <freifunk@3dfxatwork.de>
2014-11-09 15:49:15 +0000

    detect halle ips also with 10, prefix

--- netz-status/anzahl-nachbarn-halle.php
+++ netz-status/anzahl-nachbarn-halle.php
@@ -1,7 +1,7 @@
 <?php 
 require_once("functions.php");
 
-$out=shell_exec("wget -q -O - http://127.0.0.1:2006/neighbors |grep 104.62. |wc -l") ;
+$out=shell_exec("wget -q -O - http://127.0.0.1:2006/neighbors |grep -E '^10(4{0,1})\.62\.' |wc -l") ;
 if (startsWith($out,"0")) {
   $out = "<font color='#FF0000'>".$out."</font>";  
 }
